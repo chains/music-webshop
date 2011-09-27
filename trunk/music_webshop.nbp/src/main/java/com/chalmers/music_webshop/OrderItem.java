856f6035e65158f066fa7f20099e81a792a47f1e
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-09-27 13:08:01 +0000

    Created OrderItem, Product, ShoppingCart and User

---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/music_webshop/OrderItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.music_webshop;
+
+/**
+ *
+ * @author Johans
+ */
+public class OrderItem {
+    
+    private int qty;
+    private Product product;
+    
+}
