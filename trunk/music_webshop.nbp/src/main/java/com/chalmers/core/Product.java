d55d6887d752f26faad844322a50069df7ede34e
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-09-27 13:53:13 +0000

    Made two packages to separate sourcefiles

---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/core/Product.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.core;
+
+/**
+ * superclass to products
+ * @author sajohan
+ */
+
+public class Product {
+    
+
+
+
+    
+    
+    
+    
+    
+}
---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/core/ShoppingCart.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.core;
+
+import java.util.List;
+
+/**
+ *
+ * @author sajohan
+ */
+public class ShoppingCart {
+    
+    private List<OrderItem> products;
+    
+    public void addProduct(Product prod){
+    // TODO: Implement method    
+    }
+
+    public void removeProduct(String name){
+    // TODO: Implement method    
+    }
+    
+    public double getTotalCost(){
+    // TODO: Implement method  
+        return 0;
+    }
+    public List<OrderItem> getOrderItems(){
+    // TODO: Implement method 
+        return null;
+    }
+    
+    
+    
+    
+    
+}
