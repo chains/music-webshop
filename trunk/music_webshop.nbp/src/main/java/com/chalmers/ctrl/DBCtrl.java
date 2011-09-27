f5f5f8120003bff74d190574d8bec0ae77964c97
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-09-27 13:53:47 +0000

    Made two packages to separate sourcefiles

---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/DBCtrl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.ctrl;
+
+/**
+ *
+ * @author sajohan
+ */
+public interface DBCtrl {
+    
+    //TODO: implement interface
+    
+
+    
+}
---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/Database.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.ctrl;
+
+/**
+ *
+ * @author sajohan
+ */
+public class Database {
+    
+    //TODO: Implement class
+}
