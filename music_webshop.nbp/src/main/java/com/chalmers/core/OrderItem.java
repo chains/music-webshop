8bc59567d287dcad21fa156b83ebfd56fb2bef72
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-09-27 13:53:13 +0000

    Made two packages to separate sourcefiles

---
+++ music_webshop.nbp/src/main/java/com/chalmers/core/OrderItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.core;
+
+/**
+ *
+ * @author sajohan
+ */
+public class OrderItem {
+    
+    private int qty;
+    private Product product;
+    
+}
