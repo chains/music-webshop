272c029b50fafe8aaa938e0658816f043cb45597
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-07 09:22:33 +0000

    Created the interface and imported all methods from interface

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/DBCtrl.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/DBCtrl.java
@@ -4,14 +4,21 @@
  */
 package com.chalmers.ctrl;
 
+import java.util.List;
+
 /**
  *
  * @author Johan
  */
-public interface DBCtrl {
-    
-    //TODO: implement interface
-    
+public interface DBCtrl <T> {
+      
+    public void create(T t);
+    public void destroy(Long id);
+    public void edit(T t);
+    public T findEntity(Long id);
+    public List<T> findEntities();
+    public List<T> findEntities(int maxResults, int firstResult);
+    public int getEntityCount();
 
     
 }
