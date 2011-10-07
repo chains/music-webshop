c0d3ff8efa1850334f9e467f24f51b4e3b31c0b6
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-07 08:27:05 +0000

    Set up the entity classes

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/core/CD.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/core/CD.java
@@ -5,28 +5,55 @@
 package com.chalmers.core;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * CD product
  * @author Johan
  */
 
-
+@Entity
 public class CD implements Serializable {
-    
-private String name;
-private double price;
-private String category;
-private String description;
-private int noInStock;
 
-    public CD(String name, double price, String category, String description, int noInStock) {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+@Column(name = "cd_title", nullable = false, updatable = false)
+private String name;
+@Column(name = "cd_price", nullable = false)
+private double price;
+@Column(name = "cd_genre", nullable = false)
+private String genre;
+@Column(name = "cd_desc", nullable = false)
+private String description;
+@Column(name = "cd_stock", nullable = false)
+private int noInStock;
+@Column(name = "cd_playtime", nullable = false)
+private double playtime;
+
+    public CD() {
+    }
+
+    public CD(Long id, String name, double price, String genre, String description, int noInStock, double playtime) {
+        this.id = id;
         this.name = name;
         this.price = price;
-        this.category = category;
+        this.genre = genre;
         this.description = description;
         this.noInStock = noInStock;
+        this.playtime = playtime;
     }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
 
     
 }
--- trunk/music_webshop.nbp/src/main/java/com/chalmers/core/User.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/core/User.java
@@ -4,15 +4,41 @@
  */
 package com.chalmers.core;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Johan
  */
-public class User {
-    
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "user_name", nullable = false)
     private String email;
+    @Column(name = "user_passwd", nullable = false)
     private String passwd;
-    
-    private ShoppingCart cart;
+    @Column(name = "user_street", nullable = false)
+    private String street;
+    @Column(name = "user_city", nullable = false)
+    private String city;
+    @Column(name = "user_country", nullable = false)
+    private String country;
+    //Does not need shoppingcart in database. Only in session.
+    //private ShoppingCart cart;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
 }
