d6a49df2c206865c2901f55aa4b3840875e99cf2
dixtre@gmail.com <dixtre@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-07 10:32:26 +0000

    Added setters and getters for the CD class

--- music_webshop.nbp/src/main/java/com/chalmers/core/CD.java
+++ music_webshop.nbp/src/main/java/com/chalmers/core/CD.java
@@ -38,6 +38,54 @@ private double playtime;
     public CD() {
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNoInStock() {
+        return noInStock;
+    }
+
+    public void setNoInStock(int noInStock) {
+        this.noInStock = noInStock;
+    }
+
+    public double getPlaytime() {
+        return playtime;
+    }
+
+    public void setPlaytime(double playtime) {
+        this.playtime = playtime;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public CD(Long id, String name, double price, String genre, String description, int noInStock, double playtime) {
         this.id = id;
         this.name = name;
