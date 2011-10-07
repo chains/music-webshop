b8995e635670a09f0b6f59900a2292e1ca4173c3
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-07 11:52:37 +0000

    Added basic read and write operations in database for Customer and CD

--- music_webshop.nbp/src/main/java/com/chalmers/ctrl/DBCustomerCtrl.java
+++ music_webshop.nbp/src/main/java/com/chalmers/ctrl/DBCustomerCtrl.java
@@ -6,12 +6,16 @@ package com.chalmers.ctrl;
 
 import com.chalmers.core.Customer;
 import java.util.List;
+import java.util.NoSuchElementException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
 
 /**
  *
- * @author 
+ * @author Johan Sandström
  */
 public class DBCustomerCtrl implements DBCtrl<Customer>{
 
@@ -28,32 +32,85 @@ public class DBCustomerCtrl implements DBCtrl<Customer>{
     
     @Override
     public void create(Customer t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em = null;
+
+        em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
     }
 
     @Override
     public void destroy(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+
+        em = null;
+
+        em = getEntityManager();
+        em.getTransaction().begin();
+        Customer customer;
+       
+        try {
+            customer = em.getReference(Customer.class, id);
+            customer.getId();
+        } catch (EntityNotFoundException enfe) {
+            throw new NoSuchElementException();
+        }
+        em.remove(customer);
+        em.getTransaction().commit();
     }
 
     @Override
     public void edit(Customer t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+                em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            t = em.merge(t);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+
+                Long id = new Long((long) t.getId());
+
+                if (findEntity(id) == null) {
+                    throw new NoSuchElementException();
+                }
+            }
+        }
     }
 
     @Override
     public Customer findEntity(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em = getEntityManager();
+
+        return em.find(Customer.class, id);
     }
 
     @Override
     public List<Customer> findEntities() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em = getEntityManager();
+
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Customer.class));
+        Query q = em.createQuery(cq);
+ 
+        return (List<Customer>) q.getResultList();
     }
 
     @Override
     public List<Customer> findEntities(int maxResults, int firstResult) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em = getEntityManager();
+
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Customer.class));
+        Query q = em.createQuery(cq);
+
+        q.setMaxResults(maxResults);
+        q.setFirstResult(firstResult);
+
+        return (List<Customer>) q.getResultList();
     }
 
     @Override
