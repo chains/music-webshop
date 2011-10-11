6c935a3bb48a89ad68fdfc3d533d8b6343933595
dixtre@gmail.com <dixtre@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-11 12:41:40 +0000

    Added 2 backing beans to project

---
+++ music_webshop.nbp/src/main/java/com/chalmers/beans/ConfirmBackingBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.beans;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ConfirmBackingBean {
+
+    /** Creates a new instance of ConfirmBackingBean */
+    public ConfirmBackingBean() {
+    }
+}
---
+++ music_webshop.nbp/src/main/java/com/chalmers/beans/ViewCartBackingBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.beans;
+
+/**
+ *
+ * @author Daniel
+ */
+
+public class ViewCartBackingBean {
+
+    /** Creates a new instance of ViewCartBackingBean */
+    public ViewCartBackingBean() {
+    }
+}
