fe235296ec052e98b1eac7309c1911a0b06c0c12
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-11 12:48:51 +0000

    Merged faces-config. Backing beans for pay and shop created

---
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/PayBackingBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.beans;
+
+/**
+ *
+ * @author Johans
+ */
+public class PayBackingBean {
+
+    /** Creates a new instance of PayBackingBean */
+    public PayBackingBean() {
+    }
+}
