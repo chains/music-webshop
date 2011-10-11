0d19a7acce761c85a0894483371318f608ab11eb
dixtre@gmail.com <dixtre@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-11 14:13:52 +0000

    Moved all xhtml files to the same directory

---
+++ music_webshop.nbp/src/main/java/com/chalmers/beans/DateTimeSupportBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chalmers.beans;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author dannic
+ */
+
+
+
+@ManagedBean(name = "dateTime")
+@SessionScoped
+public class DateTimeSupportBean implements Serializable{
+    
+//    @ManagedProperty
+//    private String
+
+    /** Creates a new instance of DateTimeSupportBean */
+    public DateTimeSupportBean() {
+    }
+    
+    public String getCurrentDateTime(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date d = new Date();
+        return dateFormat.format(d);
+    }
+}
