404473c80e266824830552ba6ef9db1d167ad225
daek87@gmail.com <daek87@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-11 13:36:03 +0000

    Updated to Primefaces 2.2.1 Removed another unused js (lava.js) Updated namespaces in all xhtml files Added the menu to shop.

--- music_webshop.nbp/src/main/webapp/js/lava.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-            $("#1, #2, #3").lavaLamp({
-                fx: "backout", 
-                speed: 700,
-                click: function(event, menuItem) {
-                    return false;
-                }
-            });
-        });
