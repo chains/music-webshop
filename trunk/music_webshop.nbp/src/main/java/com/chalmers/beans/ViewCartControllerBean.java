b8c8de0cd3f5a6c7993f72c8323c3cfa94ebe916
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-12 16:08:40 +0000

    The buttons in viewCart work. Although a refresh is needed to update the table.

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ViewCartControllerBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ViewCartControllerBean.java
@@ -4,13 +4,35 @@
  */
 package com.chalmers.beans;
 
+import com.chalmers.core.CD;
+import javax.faces.bean.ManagedProperty;
+
 /**
  *
  * @author Johan Sandström
  */
 public class ViewCartControllerBean {
    
+    @ManagedProperty(value = "#{cartBean}")
+    private ShoppingCartModelBean cart;
+    
     /** Creates a new instance of ViewCartControllerBean */
     public ViewCartControllerBean() {
     }
+
+    public ShoppingCartModelBean getCart() {
+        return cart;
+    }
+
+    public void setCart(ShoppingCartModelBean bean) {
+        this.cart = bean;
+    }
+    
+    public void removeProduct(CD cd){
+        cart.removeProduct(cd);
+    }
+    public void removeOneProduct(CD cd){
+        cart.removeOneProduct(cd);
+    }
+    
 }
