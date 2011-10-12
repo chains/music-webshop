e003729a4293ddf06075d7fadbdad66575ac1298
dixtre@gmail.com <dixtre@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-12 16:54:47 +0000

    Added admin login page, faces navigation to admin page etc.

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/LoginBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/LoginBean.java
@@ -4,52 +4,42 @@
  */
 package com.chalmers.beans;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import org.primefaces.context.RequestContext;
-
 /**
  *
  * @author Daniel
  */
 
-public class LoginBean {  
-  
-    private String username;  
-      
-    private String password;  
-      
-    public String getUsername() {  
-        return username;  
-    }  
-  
-    public void setUsername(String username) {  
-        this.username = username;  
-    }  
-  
-    public String getPassword() {  
-        return password;  
-    }  
-  
-    public void setPassword(String password) {  
-        this.password = password;  
-    }  
-  
-    public void login(ActionEvent actionEvent) {  
-        RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg = null;  
-        boolean loggedIn = false;  
-          
-        if(username != null && username.equals("admin") && password != null  && password.equals("12345")) {  
-            loggedIn = true;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);  
-        } else {  
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+
+        boolean loggedIn = false;
+
+        if (username != null && username.equals("admin") && password != null && password.equals("12345")) {
+            loggedIn = true;
+            return "goToAdminPage";
+        } else {
             loggedIn = false;
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");  
-        }  
-          
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-        context.addCallbackParam("loggedIn", loggedIn);  
-    }  
-}  
+            return "loginFailed";
+        }
+    }
+}
