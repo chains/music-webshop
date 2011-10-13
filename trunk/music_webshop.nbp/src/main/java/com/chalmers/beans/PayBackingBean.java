3643f92aca17000d84867d9c18ca24abbf01be3b
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-13 12:53:14 +0000

    Created PayControlBean and created managedproperty for cart

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/PayBackingBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/PayBackingBean.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.chalmers.beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-
 /**
  *
  * @author Johan Sandström
@@ -17,6 +11,7 @@ import javax.faces.bean.SessionScoped;
 public class PayBackingBean {
 
     /** Creates a new instance of PayBackingBean */
-    public PayBackingBean() {
+    public PayBackingBean(){
+        
     }
 }
