485f3d73f4e63d2b480cd1ac9a2351f80dd27eb2
johan.m.sandstrom@gmail.com <johan.m.sandstrom@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-13 08:09:26 +0000

    You can now filter the shop by genre.

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ShopBackingBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ShopBackingBean.java
@@ -7,12 +7,19 @@ package com.chalmers.beans;
 import com.chalmers.core.CD;
 import com.chalmers.ctrl.DBCDControl;
 import com.chalmers.ctrl.Database;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author Johan Sandström
  */
+@ManagedBean(name = "shopBackingBean")
+@SessionScoped
 public class ShopBackingBean {
 
     private List<CD> products;
@@ -22,16 +29,32 @@ public class ShopBackingBean {
 
     /** Creates a new instance of ShopBackingBean */
     public ShopBackingBean() {
-        
-       
+
+
         dbControl = (DBCDControl) Database.getCDController();
         products = dbControl.findEntities();
     }
 
     public List<CD> getProducts() {
-        //TODO: Make it possible to return list with only specific genre.
-        products = dbControl.findEntities();
+//        products = dbControl.findEntities();
         return products;
+    }
+
+    public void setProducts(String genre) {
+        //TODO: Make it possible to return list with only specific genre.
+        List<CD> cds = dbControl.findEntities();
+        //Remove CD's with wrong genre
+        Iterator<CD> it = cds.iterator();
+        if (!genre.equalsIgnoreCase("all")) {
+            while (it.hasNext()) {
+                CD cd = it.next();
+                if (!cd.getGenre().equalsIgnoreCase(genre)) {
+                    it.remove();
+                }
+            }
+        }
+
+        products = cds;
     }
 
     public String getSelectedGenre() {
@@ -40,5 +63,14 @@ public class ShopBackingBean {
 
     public void setSelectedGenre(String selectedGenre) {
         this.selectedGenre = selectedGenre;
+        setProducts(selectedGenre);
+    }
+
+    public void setSelectedGenre() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Map requestMap = context.getExternalContext().getRequestParameterMap();
+        String genre = (String) requestMap.get("genre");
+        this.selectedGenre = genre;
+        setProducts(genre);
     }
 }
