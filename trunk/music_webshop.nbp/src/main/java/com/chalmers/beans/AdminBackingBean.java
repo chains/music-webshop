5d870f2b5b1f58ae7b24087c2f729fd3516e31c1
daek87@gmail.com <daek87@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-13 11:56:20 +0000

    Massive changes to beans to make Ajax work properly.

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/AdminBackingBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/AdminBackingBean.java
@@ -11,7 +11,7 @@ import javax.faces.bean.SessionScoped;
  *
  * @author Johan Sandström
  */
-@ManagedBean(name = "adminBackingBean")
+@ManagedBean
 @SessionScoped
 public class AdminBackingBean {
 
--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ViewCartBackingBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/ViewCartBackingBean.java
@@ -4,11 +4,16 @@
  */
 package com.chalmers.beans;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
 /**
  *
  * @author Daniel
  */
-
+@ManagedBean
+@RequestScoped
 public class ViewCartBackingBean {
     
     /** Creates a new instance of ViewCartBackingBean */
