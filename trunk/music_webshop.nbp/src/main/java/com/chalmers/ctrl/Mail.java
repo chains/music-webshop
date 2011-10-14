5eb4d4e738b31970a1e2dc0cb478126cc4a2befd
dixtre@gmail.com <dixtre@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-14 13:10:31 +0000

    Mailer now works! Also fixed viewCart buttonupdate!

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/Mail.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/Mail.java
@@ -4,9 +4,6 @@
  */
 package com.chalmers.ctrl;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Multipart;
@@ -15,9 +12,8 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimeMessage.RecipientType;
-import javax.mail.util.ByteArrayDataSource;
+import javax.mail.internet.MimeMultipart;
 import javax.naming.InitialContext;
 
 /**
@@ -33,60 +29,36 @@ public class Mail {
     }
    
     public void sendMail() throws Exception {
-        InitialContext ctx = new InitialContext();
-        Session session = (Session) ctx.lookup("mail/mailserver");
-        // Or by injection.
-        //@Resource(name = "mail/<name>")
-        //private Session session;
 
-        // Create email and headers.
+        InitialContext ctx = new InitialContext();
+        Session session =
+            (Session) ctx.lookup("mail/mailserver");
+
         Message msg = new MimeMessage(session);
-        msg.setSubject("Confirmation");
+        msg.setSubject("Confirmation email");
         msg.setRecipient(RecipientType.TO,
                          new InternetAddress(
                          "dixtre@gmail.com",
-                         "Daniel"));
+                         "Customer"));
+        
         msg.setFrom(new InternetAddress(
-                    "music.webshop.mailer@gmail.com",
-                    "Jack from Lost"));
+                    "mailer.music.webshop@gmail.com",
+                    "MailerRobot at MusicWebshop"));
 
         // Body text.
         BodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setText(message);
+        
+        messageBodyPart.setText("Here is your order confirmation " + message);
 
-//        // Multipart message.
-//        Multipart multipart = new MimeMultipart();
-//        multipart.addBodyPart(messageBodyPart);
-//
-//        // Attachment file from string.
-//        messageBodyPart = new MimeBodyPart();
-//        messageBodyPart.setFileName("README1.txt");
-//        messageBodyPart.setContent(new String(
-//                                   "file 1 content"),
-//                                   "text/plain");
-//        multipart.addBodyPart(messageBodyPart);
-//
-//        // Attachment file from file.
-//        messageBodyPart = new MimeBodyPart();
-//        messageBodyPart.setFileName("README2.txt");
-//        DataSource src = new FileDataSource("file.txt");
-//        messageBodyPart.setDataHandler(new DataHandler(src));
-//        multipart.addBodyPart(messageBodyPart);
-//
-//        // Attachment file from byte array.
-//        messageBodyPart = new MimeBodyPart();
-//        messageBodyPart.setFileName("README3.txt");
-//        src = new ByteArrayDataSource(
-//            "file 3 content".getBytes(),
-//            "text/plain");
-//        messageBodyPart.setDataHandler(new DataHandler(src));
-//        multipart.addBodyPart(messageBodyPart);
-//
-//        // Add multipart message to email.
-//        msg.setContent(multipart);
-
+         Multipart multipart = new MimeMultipart();  
+         multipart.addBodyPart(messageBodyPart); 
+   
+         // Add multipart message to email.  
+         msg.setContent(multipart); 
+        
         // Send email.
         Transport.send(msg);
+        
     }
 
 }
