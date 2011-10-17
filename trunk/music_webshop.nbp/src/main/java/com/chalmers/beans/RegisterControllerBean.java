84d75fd2e7ccb69e3c5047289219fac5628739ca
anderssonynghammar@gmail.com <anderssonynghammar@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-17 07:49:26 +0000

    Changed name of DBCustomerCtrl to DBShopUserControl

--- trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/RegisterControllerBean.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/beans/RegisterControllerBean.java
@@ -5,6 +5,7 @@
 package com.chalmers.beans;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 /**
@@ -14,8 +15,27 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class RegisterControllerBean {
+    
+    
+    @ManagedProperty(value = "#{registerBackingBean}")
+    private RegisterBackingBean registerBackingBean;
 
     /** Creates a new instance of RegisterControllerBean */
     public RegisterControllerBean() {
     }
+
+    public RegisterBackingBean getRegisterBackingBean() {
+        return registerBackingBean;
+    }
+
+    public void addShopUser(){
+        //TODO implement method
+    }
+    
+    public void setRegisterBackingBean(RegisterBackingBean registerBackingBean) {
+        this.registerBackingBean = registerBackingBean;
+    }
+    
+    
+    
 }
--- trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/Database.java
+++ trunk/music_webshop.nbp/src/main/java/com/chalmers/ctrl/Database.java
@@ -23,4 +23,5 @@ public class Database {
         return new DBCDControl(emf);
 //        return new ProductJpaController(emf);
     }
+    
 }
