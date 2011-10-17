9815ad564a7e0dd117546d92382858056dd119e5
anderssonynghammar@gmail.com <anderssonynghammar@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-17 09:19:41 +0000

    Basic loginmodule on the right side of the page.

--- music_webshop.nbp/src/main/java/com/chalmers/beans/LoginBean.java
+++ music_webshop.nbp/src/main/java/com/chalmers/beans/LoginBean.java
@@ -5,6 +5,9 @@
 package com.chalmers.beans;
 
 import com.chalmers.core.ShopUser;
+import com.chalmers.ctrl.DBShopUserControl;
+import com.chalmers.ctrl.Database;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -12,16 +15,16 @@ import javax.faces.bean.SessionScoped;
  *
  * @author Daniel Nicklasson
  */
-
 @ManagedBean
 @SessionScoped
-
 public class LoginBean {
 
     private ShopUser user;
     private String username;
     private String password;
     private boolean loggedIn;
+    private boolean isAdmin;
+    private List<ShopUser> users;
 
     public String getUsername() {
         return username;
@@ -39,8 +42,6 @@ public class LoginBean {
         this.user = user;
     }
 
-    
-    
     public String getPassword() {
         return password;
     }
@@ -51,15 +52,31 @@ public class LoginBean {
 
     public String login() {
 
-        loggedIn = false;
+        DBShopUserControl shopUserControl = (DBShopUserControl) Database.getShopUserController();
 
-        if (username != null && username.equals("admin") && password != null && password.equals("12345")) {
-            loggedIn = true;
-            return "goToAdminPage";
-        } else {
-            loggedIn = false;
-            return "loginFailed";
+        users = shopUserControl.findEntities();
+        for (ShopUser shopUser : users) {
+            if (username != null && username.equals(shopUser.getEmail()) && password != null && password.equals(shopUser.getPasswd())) {
+                loggedIn = true;
+                user = shopUser;
+                if(shopUser.getIsAdmin())
+                    isAdmin = true;
+                return "loginSuccess";
+            }
         }
+        user = null;
+        loggedIn = false;
+        isAdmin = false;
+        return "loginFailed";
+//        loggedIn = false;
+//        
+//        if (username != null && username.equals("admin") && password != null && password.equals("12345")) {
+//            loggedIn = true;
+//            return "goToAdminPage";
+//        } else {
+//            loggedIn = false;
+//            return "loginFailed";
+//        }
     }
 
     public boolean isLoggedIn() {
@@ -69,26 +86,39 @@ public class LoginBean {
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
-    
-    public String checkLogin(){
-        if(loggedIn){
-            
+
+    public String checkLogin() {
+        if (loggedIn) {
+
 //            jQuery('#loginLink').fadeOut();  
             return "Logged in as Admin";
-        }
-        else{
+        } else {
             // Return nullstring, admin not logged in
             return "";
         }
     }
-    public String buttonVisibility(){
-        if(loggedIn){
-            return "hidden";
+
+        public String loginDivVisibility() {
+        if (loggedIn) {
+            return "none";
+        } else {
+            return "block";
         }
-        else{
-            return "visible";
+    }
+        
+                public String loggedinDivVisibility() {
+        if (!loggedIn) {
+            return "none";
+        } else {
+            return "block";
         }
     }
     
-    
+    public String buttonVisibility() {
+        if (loggedIn) {
+            return "hidden";
+        } else {
+            return "visible";
+        }
+    }
 }
