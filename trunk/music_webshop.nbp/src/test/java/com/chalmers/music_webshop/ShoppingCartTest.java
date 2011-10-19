319ab1bc9dabedfbeed0d8ba457e8bb5bf7e2724
daek87@gmail.com <daek87@gmail.com@80603919-71ec-5cc7-a1a6-8f0cc6fc44e3>
2011-10-19 09:06:09 +0000

    Changed method name to small first letter instead of capital.

--- trunk/music_webshop.nbp/src/test/java/com/chalmers/music_webshop/ShoppingCartTest.java
+++ trunk/music_webshop.nbp/src/test/java/com/chalmers/music_webshop/ShoppingCartTest.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -34,7 +34,7 @@ public class ShoppingCartTest {
     
     
     @Test
-    public void TestShoppingCart(){
+    public void testShoppingCart(){
         
         cart = new ShoppingCartModelBean();
         
